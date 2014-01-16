18a345258c9355b256afc1b9d3432fb417444f57
Peechiz <cimpicci@gmail.com>
2014-01-16 16:36:11 -0500

    Update datamosher.py

--- datamosher.py
+++ datamosher.py
@@ -7,3 +7,22 @@ Each video frame in the AVI file will start with the code 00dc,
 you can search for these in the file (there will be a lot!).
 About 1 in 25 of these will be an I-Frame (though this depends on things like what quality you saved it at, etc).
 An I-Frame can be identified by the HEX string 00 01 B0 01 that will appear about 5 bytes after the 00dc frame start marker.
+
+"""
+
+#open file
+
+source_vid = raw_input("Choose a file to mosh:")
+
+readfile=open(source_vid, "r")
+
+with open(source_vid+"_moshed", "w") as outfile:
+  new_vid = "" #define new video
+  
+  iframes = [] #list of iframes
+  
+  """
+  list[0:4] returns a slice (another list) of the beginning of the list to the 5th item.
+  """
+  
+  outfile.write(new_vid)
